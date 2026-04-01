17191514ee5af702c920d612d6039f53c45bde29
Michael Barnwell <michael.barnwell@sentryinteractive.com>
2026-04-01 13:23:07 +0100

    fixes

--- android/sample-app/build.gradle.kts
+++ android/sample-app/build.gradle.kts
@@ -33,7 +33,7 @@ android {
 
 kotlin {
     compilerOptions {
-        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11)
+        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11
     }
 }
 
