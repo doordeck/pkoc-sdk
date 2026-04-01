c63d904b3389eb001406e256c570ccc7a4aea9aa
Michael Barnwell <michael.barnwell@sentryinteractive.com>
2026-04-01 14:10:14 +0100

    fixes

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -23,22 +23,3 @@ plugins {
     alias(libs.plugins.protobuf) apply false
     alias(libs.plugins.compose.compiler) apply false
 }
-
-allprojects {
-    configurations.all {
-        resolutionStrategy.eachDependency {
-            if (requested.group == "io.netty") {
-                useVersion("4.1.132.Final")
-                because("Various security fixes")
-            }
-            if (requested.group == "org.bitbucket.b_c" && requested.name == "jose4j") {
-                useVersion("0.9.6")
-                because("CVE fix: DoS via compressed JWE content (GHSA-3677-xxcr-wjqv)")
-            }
-            if (requested.group == "org.jdom" && requested.name == "jdom2") {
-                useVersion("2.0.6.1")
-                because("CVE fix: XML External Entity (XXE) Injection in JDOM")
-            }
-        }
-    }
-}
